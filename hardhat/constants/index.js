dc2a0a15b2aef70269c996bf5780032bce5f36a9
maguro <maguro.devlife@gmail.com>
2022-06-05 20:09:07 +0900

    added crypto devs contract

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x2238b12025Efb28d01d6Fec2bC979B795a4f1610";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
